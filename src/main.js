ad491a2e144fffd29a62929ef1d03b212ad41795
M <M@qq.com>
2019-04-16 22:53:52 +0800

    通过 col width 设置列宽

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import iview from "iview";
 import "iview/dist/styles/iview.css";
 
+import * as fundebug from "fundebug-javascript";
+import fundebugVue from "fundebug-vue";
+fundebug.apikey = "170eb16186b18c81c5c5dde36965b70bdfaf37d18144563ca1e6514777f527c4"
+fundebugVue(fundebug, Vue);
+
 Vue.use(iview);
 
 Vue.config.productionTip = false
